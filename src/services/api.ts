292c3fe583b77715405520ceff9bb81d997ba9d7
mohammad taheri <55748655+mohammadtaheri30132@users.noreply.github.com>
2026-07-28 11:09:39 +0330

    ad

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:5001/api'
+const API_BASE_URL = 'https://vasehkhoneh.ir/kitayar-api/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
