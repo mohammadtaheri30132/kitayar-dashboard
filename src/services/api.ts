79e1bc6df99d21cffc0f9f3595a3c556d2057658
mohammad taheri <55748655+mohammadtaheri30132@users.noreply.github.com>
2026-08-13 18:31:34 +0330

    remove distd

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const API_BASE_URL = 'https://vasehkhoneh.ir/kitayar-api/api'
-const API_BASE_URL = 'http://localhost:5001/api'
+const API_BASE_URL = 'https://vasehkhoneh.ir/kitayar-api/api'
+// const API_BASE_URL = 'http://localhost:5001/api'
 
 
 const api = axios.create({
